e09492e8a2579eb740df5ec9ebd70b9ca57ce9e9
lossany <lossany08@gmail.com>
2024-10-26 21:02:49 +0700

    update pushing

--- data/index.ts
+++
@@ -1,8 +0,0 @@
-import { INav } from "@/lib/interface/global";
-
-export const NavItems: INav[] = [
-    {id: '1', title: 'About', url: '/about'},
-    {id: '0', title: 'Uses', url: '/uses'},
-    {id: '0', title: 'Works', url: '/works'},
-    {id: '0', title: 'Contact', url: '/contact'},
-]
